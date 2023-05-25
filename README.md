717e924c729abf92934e393b14eb6eb7abc3bcfa
Kirill Shapoval <aeskelll27@gmail.com>
2023-05-25 10:59:40 +0300

    Add comments to script.js, add README.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# etch-a-sketch
+# Etch-a-Sketch
+
+*Game in which you can draw by hovering white space in the viewport*
+
+**How to play?**
+1. *Press "Choose number of squares" button.*
+2. *Choose how many squares per side you want.*
+3. *To paint, just hover white box with your mouse*
+4. *To make new layout press button "Choose number of squares"*
--- script.js
+++ script.js
@@ -1,28 +1,22 @@
+const button = document.querySelector(".button");
 const box = document.querySelector(".box");
 box.style.gridTemplateColumns = "repeat(16, 1fr)";
 box.style.gridTemplateRows = "repeat(16, 1fr)";
-const button = document.querySelector(".button");
-let gridCount = 0;
 
-for (let i = 0; i < 256; i++) {
-  let square = document.createElement("div");
-  square.style.cssText = "background: white";
-  box.insertAdjacentElement("beforeend", square);
-  square.addEventListener("click", () => {
-    square.style.background = "black";
-  });
-}
-
+//deleting divs from box
 function removeSquares() {
   while (box.firstChild) {
     box.removeChild(box.lastChild);
   }
 }
 
+//1) deletes old sketch 2)fill box with divs accordingly to what user will choose
 button.addEventListener("click", () => {
   removeSquares();
-  let answer = prompt("How many squares in line you want?(more than 2 and less than 101)");
-  if(answer > 2 && answer < 101){
+  let answer = prompt(
+    "How many squares per side you want? \n(Type one number more than 2 and less than 101)"
+  );
+  if (answer > 2 && answer < 101) {
     box.style.gridTemplateColumns = `repeat(${answer}, 1fr)`;
     box.style.gridTemplateRows = `repeat(${answer}, 1fr)`;
     for (let i = 0; i < answer * answer; i++) {
@@ -34,8 +28,6 @@ button.addEventListener("click", () => {
       });
     }
   } else {
-    alert("Choose right proportions");
+    alert("Please, choose right proportions!");
   }
 });
-
-
